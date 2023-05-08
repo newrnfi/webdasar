bc4cf370d19aae9bb6ec4e9cbadfebff106101e1
newrnfi <nailahabami@gmail.com>
2023-05-08 21:57:33 +0700

    tambah screenshot db mahasiswa di mysql

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nama = "Luffy";
+    $umur = 19;
+    echo "Welcome, ".$nama;
+    echo "<br/>";
+    echo "Kamu berusia ".$umur;
+    echo "<br/>";
+    echo "Kamu lahir di tahun ";
+    echo 2023-$umur;
+
+?>
